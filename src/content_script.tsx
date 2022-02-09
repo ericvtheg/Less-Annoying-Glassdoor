99084418c8d2374d4053bf662842f972ef13a8ce
Eric Ventor <ericventor97@gmail.com>
2022-02-08 19:30:40 -0800

    added icon

--- src/content_script.tsx
+++ src/content_script.tsx
@@ -1,9 +1,3 @@
 var s = document.createElement('script');
 s.src = chrome.runtime.getURL('./js/remove_hardsell.js');
 (document.head || document.documentElement).appendChild(s);
-
-// chrome.tabs.executeScript({file: "./remove_hardsell.js"}, function() {
-//     if(chrome.runtime.lastError) {
-//       console.error("Script injection failed: " + chrome.runtime.lastError.message);
-//     }
-//   });
